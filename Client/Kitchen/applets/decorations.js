d91f3fa417c1ef630fd74e200c6a00988af2c4e4
Kenny-Lacroix <kenny.lacroix@telenet.be>
2023-10-27 21:48:36 +0200

    something

--- Client/Kitchen/applets/decorations.js
+++ Client/Kitchen/applets/decorations.js
@@ -1,24 +1,23 @@
 //#region IMPORTS
 import "../components/lightrope/lightrope.js"
+import "../components/christmas_tree/christmas_tree.js"
 //#endregion IMPORTS
 
 //#region TEMPLATE
 const orderSystem = document.createElement('template');
-orderSystem.innerHTML = /* html */ `
-<style>
-        @import './applets/style.css';
-</style>
-<lightrope-ɮ><lightrope-ɮ/>
+orderSystem.innerHTML = 
+/* html */ `
+
+<lightrope-ɮ></lightrope-ɮ>
 `;
 //#endregion TEMPLATE
 
 //#region CLASS
-window.customElements.define('christmas-order-2023-ɮ', class extends HTMLElement {
+window.customElements.define('decorations-2023-ɮ', class extends HTMLElement {
     constructor() {
         super();
         this._shadowRoot = this.attachShadow({ 'mode': 'open' });
         this._shadowRoot.appendChild(orderSystem.content.cloneNode(true));
-
     }
 
     set content(x) {
